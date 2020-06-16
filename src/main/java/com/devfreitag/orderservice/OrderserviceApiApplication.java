9f377de9884c097d013f89d403a590077e27fb6a
Alex Freitag <alexefreitag@gmail.com>
2020-06-15 22:34:16 -0300

    create a client model and 'get' request

--- src/main/java/com/devfreitag/orderservice/OrderserviceApiApplication.java
+++ src/main/java/com/devfreitag/orderservice/OrderserviceApiApplication.java
@@ -1,4 +1,4 @@
-package com.devfreitag.orderserviceapi;
+package com.devfreitag.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
