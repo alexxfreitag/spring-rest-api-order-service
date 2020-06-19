04818a8e0161cf875d0b37920327ab87e4d88df4
Alex Freitag <alexefreitag@gmail.com>
2020-06-18 22:44:16 -0300

    retrieve clients from database instead of fixed values

--- src/main/java/com/devfreitag/orderservice/api/controller/ClienteController.java
+++ src/main/java/com/devfreitag/orderservice/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.devfreitag.orderservice.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,20 +14,11 @@ import com.devfreitag.orderservice.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Alex");
-		cliente1.setTelefone("47 99999-9999");
-		cliente1.setEmail("alex@email.com");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Freitag");
-		cliente2.setTelefone("47 99999-9999");
-		cliente2.setEmail("freitag@email.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
