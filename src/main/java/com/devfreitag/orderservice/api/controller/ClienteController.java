e10b5b49bcae6d40eddea3302494641f91eab55d
Alex Freitag <alexefreitag@gmail.com>
2020-06-18 23:18:58 -0300

    implements repository

--- src/main/java/com/devfreitag/orderservice/api/controller/ClienteController.java
+++ src/main/java/com/devfreitag/orderservice/api/controller/ClienteController.java
@@ -1,24 +1,23 @@
 package com.devfreitag.orderservice.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devfreitag.orderservice.domain.model.Cliente;
+import com.devfreitag.orderservice.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 
-	@PersistenceContext
-	private EntityManager manager;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+		return clienteRepository.findAll();
 	}
 }
---
+++ src/main/java/com/devfreitag/orderservice/domain/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.devfreitag.orderservice.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devfreitag.orderservice.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
